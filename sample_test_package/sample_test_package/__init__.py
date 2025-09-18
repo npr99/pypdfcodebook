6a51e0401b36ab9be6877025870e0b6a6a15ccb1
Nathanael Rosenheim <nproctor@tamu.edu>
2025-09-18 13:51:32 -0500

    copy repo files from pyncoda-codebook

---
+++ sample_test_package/sample_test_package/__init__.py
@@ -0,0 +1,20 @@
+def add_num(a: float, b: float) -> float:
+    """Return the sum of *a* and *b*.
+
+    Examples
+    --------
+    >>> add_num(2, 3)
+    5
+    """
+    return a + b
+
+
+def subtract_num(a: float, b: float) -> float:
+    """Return the difference of *a* and *b*.
+
+    Examples
+    --------
+    >>> subtract_num(2, 3)
+    -1
+    """
+    return a - b
