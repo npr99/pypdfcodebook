9a96c6c3642b79c3380399b17fd3205512bda56c
Nathanael Rosenheim <nproctor@tamu.edu>
2025-12-18 14:46:37 -0600

    prepare for 0.5.4 release

--- src/pypdfcodebook/__init__.py
+++ src/pypdfcodebook/__init__.py
@@ -7,7 +7,7 @@
 ******************************************************************************
 """
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 
 # For all other functions, import directly from submodules, e.g.:
 # from pypdfcodebook.simple import add_one
